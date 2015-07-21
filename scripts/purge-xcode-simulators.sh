fb2711cd6bbd70ccfce500c25434acde3c0b13a3
Tomasz Wójcik <wojcik.tomek@gmail.com>
2015-07-21 09:48:16 +0200

    Update for XCode.

--- scripts/purge-xcode-simulators.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-instruments -s devices \
- | grep Simulator \
- | grep -o "[0-9A-F]\{8\}-[0-9A-F]\{4\}-[0-9A-F]\{4\}-[0-9A-F]\{4\}-[0-9A-F]\{12\}" \
- | while read -r line ; do
-    echo "Reseting Simulator with UDID: $line"
-    xcrun simctl erase $line
-done
